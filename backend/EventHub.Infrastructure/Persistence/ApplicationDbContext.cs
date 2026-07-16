be6d92ac6e614686ed3a45792c4e02f08e1966bf
Praneet <saipraneet093@gmail.com>
2026-07-17 01:21:33 +0530

    Add User Entity and initial database migration

--- backend/EventHub.Infrastructure/Persistence/ApplicationDbContext.cs
+++ backend/EventHub.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
+using EventHub.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventHub.Infrastructure.Persistence;
 
-public class  ApplicationDbContext : DbContext
+public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
     }
+
+    public DbSet<User> Users => Set<User>();
 }
