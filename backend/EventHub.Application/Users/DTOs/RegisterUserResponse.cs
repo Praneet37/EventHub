8abc6c55de25effea2d67fd9cf14cc327b987233
Praneet <saipraneet093@gmail.com>
2026-07-17 22:25:54 +0530

    Add User registration DTOs

---
+++ backend/EventHub.Application/Users/DTOs/RegisterUserResponse.cs
@@ -0,0 +1,8 @@
+namespace EventHub.API.Users.DTOs;
+
+public class RegisterUserResponse
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+ }
