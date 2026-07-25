2be6ea813f49fb86d68ba238556357ee290110c8
Praneet <saipraneet093@gmail.com>
2026-07-25 23:45:53 +0530

    Add login request DTO and service contract

---
+++ backend/EventHub.Application/Users/DTOs/LoginUserRequest.cs
@@ -0,0 +1,7 @@
+namespace EventHub.Application.Users.DTOs;
+
+public class LoginUserRequest
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
